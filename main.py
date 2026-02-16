5154c58dea21cb0d25f4588e4cba9bf1f7088e80
RudyMontoo <rudraeng27@gmail.com>
2026-02-16 07:35:40 +0530

    Http Learning

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+import json
 app=FastAPI()
+def load_data():
+    with open('patients.json','r') as f:
+        data=json.load(f)
+    return data
 @app.get("/")
 def hello():
-    return {'message':'Hello World'}
+    return {'message':'Patient Management System API'}
 
 
 @app.get('/about')
 def about():
-    return {'message':'my name is rudra sharma'}
+    return {'message':'A fully functional API to manage your patient records'} 
+
+@app.get('/view')
+def view():
+    data=load_data()
+    return data
